c46ec33c93a13fbef1caea3cc6ed8ae3afce29c9
HaoJiang <john-jh@foxmail.com>
2022-06-14 04:35:43 +0800

    add some data files

---
+++ Scripts/featureCount2matrix.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+@infiles=@ARGV;
+my %gene_matrix,@allsamples;
+foreach $infile (@infiles){
+    open(IN,$infile) or die "$!\n";
+    <IN>;
+    $line=<IN>;
+    chomp $line;
+    @strs=split(/\s+/,$line);
+    @samples=@strs[6..$#strs];
+    for($i=0;$i<=$#samples;$i++){
+        $samples[$i] =~ s/[\/\\\.].+//;
+    }
+    push(@allsamples,@samples);
+    while($line=<IN>){
+        chomp $line;
+        @strs=split(/\s+/,$line);
+        if($strs[0] eq ""){
+            next;
+        }
+        for($i=0;$i<=$#samples;$i++){
+            $gene_matrix{$strs[0]}{$samples[$i]}=$strs[$i+6];
+        }
+    }
+}
+#@allsamples = sort(@allsamples)
+print "gene\t".join("\t",@allsamples)."\n";
+foreach $gene (sort keys(%gene_matrix)){
+    print $gene;
+    foreach $sample (@allsamples){
+        if(exists($gene_matrix{$gene}{$sample})){
+            print "\t".$gene_matrix{$gene}{$sample};
+        }else{
+            print "\t0";
+        }
+    }
+    print "\n";
+} 
